9ffef8a18aaf5d47e37d625b496299101b3181bc
Ben Lavalley <ben.lavalley@kaseya.com>
2019-10-29 02:20:06 -0400

    version bump

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: 'tmeasday:check-npm-versions',
-  version: '0.3.2',
+  version: '0.3.3',
   summary: 'Check that required npm packages are installed at the app level',
   git: 'https://github.com/tmeasday/check-npm-versions.git',
   documentation: 'README.md',
 });
 
-Npm.depends({ semver: '5.1.0' });
+Npm.depends({ semver: '6.3.0' });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3-beta.11');
+  api.versionsFrom('1.8.1');
   api.use('ecmascript');
   api.mainModule('check-npm-versions.js');
 });
